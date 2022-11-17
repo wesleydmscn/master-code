c643a8a60bec8abdc0c2d70efccfd2c8083b321e
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-11-17 15:27:59 -0300

    fix: remove unused folders

--- contents/frontend/javascript/js-dom/get-full-name-from-inputs/solution.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('button');
-
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const fullName = document.getElementById('fullName');
-
-button.addEventListener('click' , () => {
-  fullName.value = `${firstName.value} ${lastName.value}`
-});
--- contents/frontend/javascript/js-dom/pop-the-balloons/solution.js
+++
@@ -1,6 +0,0 @@
-const list = document.getElementById('list');
-const li = list.querySelectorAll('li');
-
-[...li].map(item => item.addEventListener('mouseover', () => {
-   item.style.visibility = "hidden";
-}))
