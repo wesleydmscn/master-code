9d9f4e4981f5ac3ad4ea6090051119731f6220a9
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-11-22 15:40:20 -0300

    docs(algorithms/the-hashtag-generator): add test_cases

--- contents/algorithms/the-hashtag-generator/solution.js
+++ contents/algorithms/the-hashtag-generator/solution.js
@@ -14,3 +14,12 @@ function generateHashtag(str) {
     return hash.join('');
   }
 }
+
+const test_cases = {
+  "case 1": generateHashtag(""),
+  "case 2": generateHashtag(" ".repeat(200)),
+  "case 3": generateHashtag("Do We have A Hashtag"),
+  "case 4": generateHashtag("Codewars")
+}
+
+console.log(test_cases);
