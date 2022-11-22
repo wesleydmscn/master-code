41111fb3df329f1d9eecf0b6d0133b1458f6e54a
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-11-22 15:48:22 -0300

    docs(data-structures/moving-zeros-to-the-end): add test_cases

--- contents/data-structures/moving-zeros-to-the-end/solution.js
+++ contents/data-structures/moving-zeros-to-the-end/solution.js
@@ -3,3 +3,12 @@ function moveZeros(arr) {
           .filter(item => item !== 0)
           .concat(arr.filter(item => item === 0));
 }
+
+const test_cases = {
+  "case 1": moveZeros([1,2,0,1,0,1,0,3,0,1]),
+  "case 2": moveZeros([1,2,2,1,0,1,0,3,0,1]),
+  "case 3": moveZeros([1,2,0,1,3,1,0,3,0,1]),
+  "case 4": moveZeros([1,2,0,1,5,1,0,3,0,1])
+}
+
+console.log(test_cases);
--- contents/data-structures/moving-zeros-to-the-end/solution.py
+++ contents/data-structures/moving-zeros-to-the-end/solution.py
@@ -7,3 +7,12 @@ def move_zeros(lst):
         else: other_values.append(item)
         
     return other_values + zeros
+
+test_cases = {
+  "case 1": move_zeros([1,2,0,1,0,1,0,3,0,1]),
+  "case 2": move_zeros([1,2,2,1,0,1,0,3,0,1]),
+  "case 3": move_zeros([1,2,0,1,3,1,0,3,0,1]),
+  "case 4": move_zeros([1,2,0,1,5,1,0,3,0,1])
+}
+
+print(test_cases)
